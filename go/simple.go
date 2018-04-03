8577b39f223c25225226ff68bc021d9947d895f4
Eugene Lisitsky <lisitsky@gmail.com>
2018-04-03 11:17:07 +0300

    Take CUSTOMER, ZONE, PASSWORD, USERNAME from environment if set. Check error and add defer.

--- go/simple.go
+++ go/simple.go
@@ -8,20 +8,36 @@ import (
 	"math/rand"
 	"net/http"
 	"net/url"
+	"os"
 	"time"
 )
 
-const (
-	username         = "lum-customer-CUSTOMER-zone-ZONE"
+var (
+	customer         = "CUSTOMER"
+	zone             = "ZONE"
 	password         = "PASSWORD"
 	port             = 22225
+	username         = "lum-customer-%v-zone-%v" // "lum-customer-CUSTOMER-zone-ZONE"
 	proxyTemplateURL = "http://%v-session-%v:%v@zproxy.luminati.io:%v"
 )
 
 func main() {
+	if c := os.Getenv("CUSTOMER"); c != "" {
+		customer = c
+	}
+	if z := os.Getenv("ZONE"); z != "" {
+		zone = z
+	}
+	if p := os.Getenv("PASSWORD"); p != "" {
+		password = p
+	}
+	username = fmt.Sprintf(username, customer, zone)
+	if u := os.Getenv("USERNAME"); u != "" {
+		username = u
+	}
 	rand.Seed(time.Now().UnixNano())
-	session_id := rand.Intn(10000)
-	proxyUrlStr := fmt.Sprintf(proxyTemplateURL, username, session_id, password, port)
+	sessionId := rand.Intn(10000)
+	proxyUrlStr := fmt.Sprintf(proxyTemplateURL, username, sessionId, password, port)
 	proxyUrl, err := url.Parse(proxyUrlStr)
 	if err != nil {
 		log.Fatalf("Error parsing proxy URL: %v", err)
@@ -34,7 +50,11 @@ func main() {
 	req, err := http.NewRequest("GET", "http://lumtest.com/myip.json", nil)
 	req.Header.Add("Accept", "text/plain")
 	resp, err := httpClient.Do(req)
-	log.Printf("Resp: %+v, err %v", resp, err)
+	if err != nil {
+		log.Fatalf("Cannot get data: %v", err)
+	}
+	defer resp.Body.Close()
+	log.Printf("Response: %+v, err %v", resp, err)
 	body, err := ioutil.ReadAll(resp.Body)
 	log.Printf("Got data: %s", body)
 }
